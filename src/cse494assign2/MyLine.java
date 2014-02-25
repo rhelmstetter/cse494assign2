5f5d9e4be3eed96cec9ff141c9339430de737ac9
Rachel Helmstetter <Rachel.Helmstetter@gmail.com>
2014-02-24 18:31:32 -0700

    implemented MyLine

--- src/cse494assign2/MyLine.java
+++ src/cse494assign2/MyLine.java
@@ -11,9 +11,30 @@ import java.awt.Graphics;
 
 public class MyLine extends MyShape {
 
+	private int x2, y2;
+	
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(new Color(red, green, blue));
+		g.drawLine(x, y, x2, y2);
 	}
+
+	public int getX2() {
+		return x2;
+	}
+
+	public void setX2(int x2) {
+		this.x2 = x2;
+	}
+
+	public int getY2() {
+		return y2;
+	}
+
+	public void setY2(int y2) {
+		this.y2 = y2;
+	}
+	
+	
 
 }
